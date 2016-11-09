21fda0ff432d31de4c04103ea399e8bc84b5fedc
lrlna <shestak.irina@gmail.com>
2016-11-09 05:15:11 +0000

    actually switch to mississippi to handle streams

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 #! /usr/bin/env node
 
-const pull = require("pull-stream")
-const argv = require("yargs")
+const miss = require("mississippi")
+const path = require("path")
+const fs = require("fs")
+const yargs = require("yargs")
   .command({
     command: 'run',
-    desc: 'Run readyou',
-    handler: readyou()
+    desc: 'Generate a readme by providing a .json file',
+    builder: {
+      file: {
+        desc: 'provide a json file',
+      }
+    },
+    handler: (argv) => {
+      var output = path.join(process.cwd(), 'README.md')
+      var read = fs.createReadStream(argv.file)
+      var write = fs.createWriteStream(output)
+
+      // create a read stream to read pckg.json
+      miss.pipe(read, readyou, write, function (err) {
+        if (err) return console.error(`Can't process json`)
+        console.log(`file written successfully`)
+      })
+    }
   })
   .demand(1)
   .help()
   .argv
 
-function readyou(argv) {
-  var from = argv.from
-  // stream data from .json file to a .md file
-  pull(getData(), transformData(), writeData())
-}
-
-// create a read stream to read pckg.json
-function getData(file) {
-  return function(read) {
-    return function (err, callback) {
-      if (!file--) return read(true, callback)
-      read(null, callback)
-    }
-  }
+var readyou = function() {
+  
 }
 
 // keys are headings, values are filler text if there are any
-function transformData() {
-}
-
-function writeData() {
-}
 
 // read current path, and create a file in current path
 
