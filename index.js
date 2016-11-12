03936aa1e76541f137eeae61fa6ae4791bfea850
lrlna <shestak.irina@gmail.com>
2016-11-12 15:47:50 +0900

    use mississippi to start a through stream to create readme

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
 #! /usr/bin/env node
 
-const miss = require("mississippi")
-const path = require("path")
-const fs = require("fs")
 const yargs = require("yargs")
-  .command({
-    command: 'run',
-    desc: 'Generate a readme by providing a .json file',
-    builder: {
-      file: {
-        desc: 'provide a json file',
-      }
-    },
-    handler: (argv) => {
-      var output = path.join(process.cwd(), 'README.md')
-      var read = fs.createReadStream(argv.file)
-      var write = fs.createWriteStream(output)
-
-      // create a read stream to read pckg.json
-      miss.pipe(read, readyou, write, function (err) {
-        if (err) return console.error(`Can't process json`)
-        console.log(`file written successfully`)
-      })
-    }
-  })
+  .command(require('run'))
   .demand(1)
   .help()
   .argv
-
-var readyou = function() {
-  
-}
-
-// keys are headings, values are filler text if there are any
-
-// read current path, and create a file in current path
 
 // if path is provided instead, create the file in that path
 
---
+++ run.js
@@ -0,0 +1,44 @@
+const miss = require("mississippi")
+const path = require("path")
+const fs = require("fs")
+
+exports.command = 'run [file]'
+
+exports.describe = 'Generate a readme by providing a .json file'
+
+exports.builder = {
+  file: {
+    desc: 'provide a json file',
+    alias: 'f'
+  }
+}
+
+exports.handler = function (argv) {
+  var input = path.join(process.cwd(), argv.file)
+  // read current path, and create a file in current path
+  var output = path.join(process.cwd(), 'README.md')
+  var read = fs.createReadStream(input)
+  var write = fs.createWriteStream(output)
+
+  var readyou = miss.through(
+    function (chunk, enc, callback) {
+      var text = ''
+      var file = JSON.parse(chunk.toString())
+      Object.keys(file).forEach(function (key) {
+        // keys are headings, values are filler text if there are any
+        text = text.concat(`# ${key} \n ${file[key]} \n`)
+      })
+      callback(null, text)
+    },
+    // last run
+    function (callback) {
+      callback(null, 'last run')
+    }
+  )
+
+  // create a read stream to read pckg.json
+  miss.pipe(read, readyou, write, function (err) {
+    if (err) return console.error(`Can't process json ${err}`)
+    console.log(`file written successfully`)
+  })
+}
