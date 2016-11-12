5d0d16401a6b85534d26083c64041fa09d49ad5a
lrlna <shestak.irina@gmail.com>
2016-11-12 22:52:17 +0900

    set up a file copy for when path is provided by user

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const miss = require("mississippi")
 const path = require("path")
+const prompt = require("prompt")
 
 exports.command = 'config [--path]'
 
@@ -14,6 +15,17 @@ exports.builder = {
 }
 
 exports.handler = function (argv) {
-  console.log(argv)
+  var path = argv.path
+  // stream the file to copy it to root
+  var read = fs.createReadStream(path)
+  var write = fs.createWriteStream('~/readyou-config.json')
+  miss.pipe(read, write, function (err) {
+    if (err) return console.error(`Can't setupt config file ${err}`)
+    console.log(`Successfully setup config file`)
+  })
 }
 
+function copyFileToRoot () {
+  // copy files from the path provided into root directory
+  // little loopy thingy while waiting for file to copy
+}
